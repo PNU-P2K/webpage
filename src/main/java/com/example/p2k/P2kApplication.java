a0ce6f80b76f293c6ee403659896e96e094b4928
suuding <tngus4789@naver.com>
2023-07-27 02:42:56 +0900

    feat : bootstrap 사용해 메인 페이지 초안 개발

---
+++ src/main/java/com/example/p2k/P2kApplication.java
@@ -0,0 +1,13 @@
+package com.example.p2k;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class P2kApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(P2kApplication.class, args);
+	}
+
+}
