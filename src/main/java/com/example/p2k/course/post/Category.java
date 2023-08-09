556c035858a2dfead5b0f51e4cba4afc8429ac72
xcelxlorx <xcelxlorx@gmail.com>
2023-08-09 21:37:27 +0900

    feat: Post 엔티티 구현
    -Category 필드는 enum으로 구현

---
+++ src/main/java/com/example/p2k/course/post/Category.java
@@ -0,0 +1,5 @@
+package com.example.p2k.course.post;
+
+public enum Category {
+    NOTICE, QUESTION, FREE
+}
---
+++ src/main/java/com/example/p2k/course/post/PostRepository.java
@@ -0,0 +1,30 @@
+package com.example.p2k.course.post;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface PostRepository extends JpaRepository<Post, Long> {
+
+    @Query("select p from Post p join fetch p.user where p.id = :postId")
+    Optional<Post> findById(@Param("postId") Long postId);
+
+    @Query("select p from Post p where p.course.id = :courseId and p.category = :category")
+    List<Post> findPostByCategory(@Param("courseId") Long courseId, @Param("category") Category category);
+
+    @Modifying(clearAutomatically = true)
+    @Query("update Post p SET p.title = :title, p.content = :content where p.id = :postId")
+    void update(@Param("title") String title, @Param("content") String content, @Param("postId") Long postId);
+
+    @Modifying
+    @Query("delete from Post p where p.id = :postId")
+    void deleteById(@Param("postId") Long postId);
+
+    @Modifying
+    @Query("delete from Post p where p.course.id = :courseId")
+    void deleteAllByCourseId(@Param("courseId") Long courseId);
+}
