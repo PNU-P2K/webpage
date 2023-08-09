95765f927712ce283438f50668c8d68d709c6872
suuding <tngus4789@naver.com>
2023-08-10 03:11:46 +0900

    chore : 로그인 프론트, 기능 수정

--- src/main/resources/static/js/scripts.js
+++ src/main/resources/static/js/scripts.js
@@ -7,67 +7,31 @@
 // Scripts
 //
 
-function subminJoin() {
-    alert("회원가입");
-    let data = {
-        id: $("#id").val(),
-        password: $("#password").val(),
-        username: $("#name").val(),
-        email: $("#email").val(),
-        role: $("#role").val()
-    }
+
+// $('#email_check').on(function () {
+//     var email = $('#email').val();
+//     alert("check");
+//
+//     $.ajax({
+//         type : "post",
+//         url : "/user/check",
+//         data : email
+//     })
+// });
+
+function email_chk() {
+
+    var email = $('#email').val();
+    alert("check");
+    console.log("check_log"+email);
 
     $.ajax({
-        // 회원가입 수행 요청
-        type: "POST",
-        url: "/join",
-        data: JSON.stringify(data), // http body 데이터
-        contentType: "application/json; charset=utf-8", // body 데이터가 어떤 타입인지 (MIME)
-        dataType: "json" // 요청을 서버로 해서 응답이 왔을 때 기본적으로 모든 것이 String(문자열), 만약 생긴게 json이라면 javascript 오브젝트로 변경
-    }).done(function (resp) {
-        alert("회원가입이 완료되었습니다.");
-        location.href = "/login";
-    }).fail(function (error) {
-        alert("회원가입이 실패하였습니다.");
-        alert(JSON.stringify(error));
-    });
+        type : "post",
+        url : "/user/check",
+        data : {"email" : email},
+        dataType : "JSON"
+    })
 }
-
-let index = {
-    init: function () {
-        $("#join-btn").on("submit", ()=>{ // function(){} 대신 ()=>{} 를 쓴 이유 : this를 바인딩하기 위해서
-            this.save();
-        });
-    },
-
-    save: function () {
-        alert("회원가입");
-        let data = {
-            id: $("#id").val(),
-            password: $("#password").val(),
-            username: $("#name").val(),
-            email: $("#email").val(),
-            role: $("#role").val()
-        }
-
-        $.ajax({
-            // 회원가입 수행 요청
-            type: "POST",
-            url: "/join",
-            data: JSON.stringify(data), // http body 데이터
-            contentType: "application/json; charset=utf-8", // body 데이터가 어떤 타입인지 (MIME)
-            dataType: "json" // 요청을 서버로 해서 응답이 왔을 때 기본적으로 모든 것이 String(문자열), 만약 생긴게 json이라면 javascript 오브젝트로 변경
-        }).done(function (resp) {
-            alert("회원가입이 완료되었습니다.");
-            location.href = "/login";
-        }).fail(function (error) {
-            alert("회원가입이 실패하였습니다.");
-            alert(JSON.stringify(error));
-        });
-    }
-}
-
-index.init();
 
 window.addEventListener('DOMContentLoaded', event => {
 
