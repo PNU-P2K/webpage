efc0d404a9c688fb37d0352d2b7ee5224c9a257d
xcelxlorx <xcelxlorx@gmail.com>
2023-08-22 14:26:05 +0900

    feat: 홈 화면 설정
    로그인 전 화면 - index.html
    로그인 후 화면 - index-after.html
    현재는 api가 다르게 구현되어 있지만 수정할 예정

--- src/main/java/com/example/p2k/_core/security/SecurityConfig.java
+++ src/main/java/com/example/p2k/_core/security/SecurityConfig.java
@@ -4,7 +4,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
@@ -32,7 +31,7 @@ public class SecurityConfig {
                                 .loginPage("/user/login")
                                 .loginProcessingUrl("/user/login")
                                 .usernameParameter("email")
-                                .defaultSuccessUrl("/vm", true)
+                                .defaultSuccessUrl("/home", true)
                                 .permitAll()
                 )
                 .logout(
