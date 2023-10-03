95045133af976fe001011458185d478860c1728b
suuding <tngus4789@naver.com>
2023-10-03 13:49:50 +0900

    fix : exception 추가

---
+++ src/main/java/com/example/p2k/_core/exception/Exception403.java
@@ -0,0 +1,22 @@
+package com.example.tily._core.errors.exception;
+
+import com.example.tily._core.utils.ApiUtils;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+
+// 권한 없음
+@Getter
+public class Exception403 extends RuntimeException {
+    public Exception403(String message) {
+        super(message);
+    }
+
+    public ApiUtils.ApiResult<?> body(){
+        return ApiUtils.error(getMessage(), HttpStatus.FORBIDDEN);
+    }
+
+    public HttpStatus status(){
+        return HttpStatus.FORBIDDEN;
+    }
+}
