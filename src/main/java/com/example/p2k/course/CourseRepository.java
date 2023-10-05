e6e876dad4af817df3c4887487fd3d9acd408248
xcelxlorx <xcelxlorx@gmail.com>
2023-10-06 03:22:59 +0900

    feat: 강좌 신청 확인 알림창

--- src/main/java/com/example/p2k/course/CourseRepository.java
+++ src/main/java/com/example/p2k/course/CourseRepository.java
@@ -3,9 +3,8 @@ package com.example.p2k.course;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.query.Param;
 
 public interface CourseRepository extends JpaRepository<Course, Long> {
 
-    Page<Course> findByNameContainingIgnoreCase(Pageable pageable, @Param("keyword") String keyword);
+    Page<Course> findByNameContainingIgnoreCase(Pageable pageable, String keyword);
 }
