2de0ac93651be68a8eb38036d9daa7f3ed614d7f
xcelxlorx <xcelxlorx@gmail.com>
2023-10-09 12:49:23 +0900

    feat: s3 메트릭 가져오기

--- src/main/java/com/example/p2k/admin/AdminController.java
+++ src/main/java/com/example/p2k/admin/AdminController.java
@@ -18,39 +18,12 @@ public class AdminController {
 
     @GetMapping("/resources")
     public String manageResources(Model model, @AuthenticationPrincipal CustomUserDetails userDetails){
-        MetricDataResponse cpuUtilization = cloudWatchService.getCPUUtilization();
-        MetricDataResponse statusCheckFailedSystem = cloudWatchService.getStatusCheckFailedSystem();
-        MetricDataResponse statusCheckFailedInstance = cloudWatchService.getStatusCheckFailedInstance();
-        MetricDataResponse networkIn = cloudWatchService.getNetworkIn();
-        MetricDataResponse networkOut = cloudWatchService.getNetworkOut();
-        MetricDataResponse networkPacketsIn = cloudWatchService.getNetworkPacketsIn();
-        MetricDataResponse networkPacketsOut = cloudWatchService.getNetworkPacketsOut();
-        MetricDataResponse diskReads = cloudWatchService.getDiskReads();
-        MetricDataResponse diskReadOperations = cloudWatchService.getDiskReadOperations();
-        MetricDataResponse diskWrites = cloudWatchService.getDiskWrites();
-        MetricDataResponse diskWriteOperations = cloudWatchService.getDiskWriteOperations();
-        model.addAttribute("cpuTimestamp", cpuUtilization.getTimestamps());
-        model.addAttribute("cpuValue", cpuUtilization.getValue());
-        model.addAttribute("statusSystemTimestamp", statusCheckFailedSystem.getTimestamps());
-        model.addAttribute("statusSystemValue", statusCheckFailedSystem.getValue());
-        model.addAttribute("statusInstanceTimestamp", statusCheckFailedInstance.getTimestamps());
-        model.addAttribute("statusInstanceValue", statusCheckFailedInstance.getValue());
-        model.addAttribute("networkInTimestamp", networkIn.getTimestamps());
-        model.addAttribute("networkInValue", networkIn.getValue());
-        model.addAttribute("networkOutTimestamp", networkOut.getTimestamps());
-        model.addAttribute("networkOutValue", networkOut.getValue());
-        model.addAttribute("networkPacketsInTimestamp", networkPacketsIn.getTimestamps());
-        model.addAttribute("networkPacketsInValue", networkPacketsIn.getValue());
-        model.addAttribute("networkPacketsOutTimestamp", networkPacketsOut.getTimestamps());
-        model.addAttribute("networkPacketsOutValue", networkPacketsOut.getValue());
-        model.addAttribute("diskReadsTimestamp", diskReads.getTimestamps());
-        model.addAttribute("diskReadsValue", diskReads.getValue());
-        model.addAttribute("diskReadOpsTimestamp", diskReadOperations.getTimestamps());
-        model.addAttribute("diskReadOpsValue", diskReadOperations.getValue());
-        model.addAttribute("diskWritesTimestamp", diskWrites.getTimestamps());
-        model.addAttribute("diskWritesValue", diskWrites.getValue());
-        model.addAttribute("diskWriteOpsTimestamp", diskWriteOperations.getTimestamps());
-        model.addAttribute("diskWriteOpsValue", diskWriteOperations.getValue());
+        MetricDataResponse bucketSize = cloudWatchService.getS3BucketSize();
+        MetricDataResponse numberOfObjects = cloudWatchService.getS3NumberOfObjects();
+        model.addAttribute("bucketSizeTimestamp", bucketSize.getTimestamps());
+        model.addAttribute("bucketSizeValue", bucketSize.getValue());
+        model.addAttribute("numberOfObjectsTimestamp", numberOfObjects.getTimestamps());
+        model.addAttribute("numberOfObjectsValue", numberOfObjects.getValue());
         model.addAttribute("user", userDetails.getUser());
         return "admin/resources";
     }
