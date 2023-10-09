6c87fbd04f90a722115e994038aad60c032ec464
xcelxlorx <xcelxlorx@gmail.com>
2023-10-09 13:31:57 +0900

    feat: vm 더미데이터에 nodeport 추가

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -15,12 +15,12 @@ insert into course_tb(name, description, instructor_id) values('알고리즘', '
 insert into course_tb(name, description, instructor_id) values('DB', '3-2', 4);
 insert into course_tb(name, description, instructor_id) values('사물인터넷', '4-2', 4);
 
-insert into vm_tb(user_id, vmname, password, course_id, port, scope, control, state, description) values(1, '알고리즘', '1234' ,1, 6000, 1, 1, 'stop', '이 가상환경은 알고리즘 수업을 위한 가상환경이다');
-insert into vm_tb(user_id, vmname, password, course_id, port, scope, control, state, description) values(1, '알고리즘1', '1234' ,1, 6001, 1, 1, 'running', '이 가상환경은 알고리즘 수업을 위한 가상환경이다');
-insert into vm_tb(user_id, vmname, course_id, port, scope, control, state, description) values(2, '알고리즘059', 1, 6001, 1, 0, 'running', '알고리즘 059수업');
-insert into vm_tb(user_id, vmname, course_id, port, scope, control, state, description) values(2, '네트워크', 1, 6002, 1, 1, 'stop', '이 가상환경은 네트워크 공부를 위한 가상환경이다. 지금은 사용하지 않고 있지만, 추후에 많이 수정할 예정이다.');
-insert into vm_tb(user_id, vmname, course_id, port, scope, control, state, description) values(3, '자료구조', 1, 6003, 1, 1, 'running', '이 가상환경은 자료구조를 위해 만든 가상환경이다. 아무나 확인할 수 있다.');
-insert into vm_tb(user_id, vmname, course_id, port, scope, control, state, description) values(4, '알고리즘(교육자)', 1, 6004, 1, 1, 'stop', '이 가상환경은 알고리즘 수업을 위한 교육자의 가상환경이다');
+insert into vm_tb(user_id, vmname, password, course_id, port, scope, control, state, description, node_port) values(1, '알고리즘', '1234' ,1, 6000, 1, 1, 'stop', '이 가상환경은 알고리즘 수업을 위한 가상환경이다', 30000);
+insert into vm_tb(user_id, vmname, password, course_id, port, scope, control, state, description, node_port) values(1, '알고리즘1', '1234' ,1, 6001, 1, 1, 'running', '이 가상환경은 알고리즘 수업을 위한 가상환경이다', 30001);
+insert into vm_tb(user_id, vmname, course_id, port, scope, control, state, description, node_port) values(2, '알고리즘059', 1, 6001, 1, 0, 'running', '알고리즘 059수업', 30002);
+insert into vm_tb(user_id, vmname, course_id, port, scope, control, state, description, node_port) values(2, '네트워크', 1, 6002, 1, 1, 'stop', '이 가상환경은 네트워크 공부를 위한 가상환경이다. 지금은 사용하지 않고 있지만, 추후에 많이 수정할 예정이다.', 30003);
+insert into vm_tb(user_id, vmname, course_id, port, scope, control, state, description, node_port) values(3, '자료구조', 1, 6003, 1, 1, 'running', '이 가상환경은 자료구조를 위해 만든 가상환경이다. 아무나 확인할 수 있다.', 30004);
+insert into vm_tb(user_id, vmname, course_id, port, scope, control, state, description, node_port) values(4, '알고리즘(교육자)', 1, 6004, 1, 1, 'stop', '이 가상환경은 알고리즘 수업을 위한 교육자의 가상환경이다', 30005);
 
 
 --insert into vm_tb(user_id, vmname, course_id, port, scope, control, state) values(2, '자료구조', 1, 6086, 0, 1, 'stop');
