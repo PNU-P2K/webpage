32dc60091d94bc5952c3252641800d1a18bb7188
xcelxlorx <xcelxlorx@gmail.com>
2023-10-22 13:29:47 +0900

    refactor: CustomUserDetails import 제거, return문 수정

--- src/main/java/com/example/p2k/_core/security/CustomUserDetails.java
+++ src/main/java/com/example/p2k/_core/security/CustomUserDetails.java
@@ -1,14 +1,11 @@
 package com.example.p2k._core.security;
 
 import com.example.p2k.user.User;
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
-import java.util.List;
 
 public class CustomUserDetails implements UserDetails {
 
@@ -54,7 +51,6 @@ public class CustomUserDetails implements UserDetails {
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        List<GrantedAuthority> authorityList = AuthorityUtils.createAuthorityList(user.getRole().toString());
-        return authorityList;
+        return AuthorityUtils.createAuthorityList(user.getRole().toString());
     }
 }
