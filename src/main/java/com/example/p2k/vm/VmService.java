490edf19374b56be7da08a2a03d005cd59268b19
sososo0 <sh0000@pusan.ac.kr>
2023-10-24 17:56:32 +0900

    fix: Change create base image in create

--- src/main/java/com/example/p2k/vm/VmService.java
+++ src/main/java/com/example/p2k/vm/VmService.java
@@ -36,8 +36,8 @@ public class VmService {
     private int nodePort = 30000; // 30000~32768까지 사용 가능
     private final String baseImagePath = "registry.p2kcloud.com/base/";
 
-    //private final String baseURL = "http://175.45.203.51:5000"; // k8s에게 명령을 내리는 서버 - test용
-    private final String baseURL = "http://223.130.137.170:5000"; // k8s에게 명령을 내리는 서버 - 실제 서버용
+    private final String baseURL = "http://175.45.203.51:5000"; // k8s에게 명령을 내리는 서버 - test용
+    //private final String baseURL = "http://223.130.137.170:5000"; // k8s에게 명령을 내리는 서버 - 실제 서버용
     //private final String baseURL = "http://localhost:5000";
 
     //private final String baseURL = "http://3.37.62.95:5000";
@@ -110,7 +110,7 @@ public class VmService {
         // 암호화한 key
         String key = bCryptPasswordEncoder.encode(user.getPassword());
 
-        String imagePath = baseImagePath+1+"/kasmweb:1.14.0";; // create시에는 base/1을 기준으로 생성
+        String imagePath = baseImagePath+1+"/kasmweb:v1";; // create시에는 base/1을 기준으로 생성
 
         // requestDTO header 설정
         HttpHeaders headers = new HttpHeaders();
